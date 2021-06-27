a4562b5acc0e39dde1cb3380ed04e9e76b011ee8
lknutstad <lknutstad@gmail.com>
2021-06-26 17:27:25 -0700

    queries completed

--- queries/Employee_Database_Challenge.sql
+++ queries/Employee_Database_Challenge.sql
@@ -141,7 +141,6 @@ ON (de.dept_no = d.dept_no)
 WHERE d.dept_name IN('Development', 'Sales');
 
 -- Use Dictinct with Orderby to remove duplicate rows
--- SELECT DISTINCT ON (______) ...,
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -173,3 +172,21 @@ FROM unique_titles as ut
 GROUP BY title 
 ORDER BY count DESC;
 
+-- Create a membership eligibility table
+SELECT DISTINCT ON (e.emp_no) 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+--INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN "Titles" AS ti
+ON (de.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;													
